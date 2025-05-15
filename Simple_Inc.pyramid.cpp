97a88878158af3d00e921850fdd7ae70cc3fd462
Retr00010000 <lackingfot@gmail.com>
2025-05-16 02:34:14 +0300

    Update Simple_Inc.pyramid.cpp
    first step to shapes

--- Simple_Inc.pyramid.cpp
+++ Simple_Inc.pyramid.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+using namespace std;
 int main()
 {
 	int n;
@@ -7,9 +9,9 @@ int main()
 if (n>1){
 	for(int i=1;i<=n;i++){      // (i and j) are changable variables...
 		for(int j=0;j<i;j++)   //  When the first loop's conditions are met the second loops comes in play.
-		{                     //  note: In loops its check codition, if correct then it's action then update. After the udpate the loop rechecks the condition to take action or not. If the condition isn't correct we escape the loop.
+		{                     //  note: In loops, it's  check the condition, if correct then it's action, and then update. After the update, the loop rechecks the condition to take action or not. If the condition isn't correct we escape the loop.
 			cout<<"*";       //  the action of the first loop IS the second loop.
-		}                   // (1)the second loop performs it's action..(2) updates but the coditions aren't met...(3) we escape the second loop and perform the last first loop's action.
+		}                   // (1)the second loop performs its action..(2) Then it updates, but the conditions aren't met...(3) we escape the second loop and perform the last first loop's action.
 		cout<<endl;        // (4) the first loop updates and rechecks after all the actions are done.
 	}
 }
